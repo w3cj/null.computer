ec009c23da58807ae7c567f51612785b11632ad3
CJ R <hello@cjr.co.de>
2017-01-01 10:09:40 -0700

    :sparkles: Add links; Auto scroll on load

--- js/posts.js
+++ js/posts.js
@@ -8,15 +8,25 @@ const ghBlog = new GHBlog('w3cj', 'null.computer');
 
 ghBlog
   .getPosts()
+  .then(addPostLinks)
   .then(getPostsHTML)
-  .then(setPostsHTML);
+  .then(setPostsHTML)
+  .then(navigateToHash);
+
+function addPostLinks(posts) {
+  const links = posts.reduce((all, post) => {
+    return `${all}<li><a href="#${post.path}">${post.path}</a></li>`;
+  }, '');
+  document.querySelector('.post-links').innerHTML = links;
+  return posts;
+};
 
 function getPostsHTML(posts) {
   return posts
           .reduce((html, post) => {
             return `
             ${html}
-            <section>
+            <section id="${post.path}">
               <small><strong>Path:</strong> ${post.path}</small><br>
               <small><strong>Logged:</strong> ${post.commits.created}</small>
               ${ post.commits.updated == post.commits.created ? '' :
@@ -30,4 +40,10 @@ function getPostsHTML(posts) {
 function setPostsHTML(html) {
   localStorage.postsHTML = html;
   document.querySelector('#posts').innerHTML = html;
+}
+
+function navigateToHash() {
+  const currentHash = window.location.hash;
+  window.location.hash = '';
+  window.location.hash = currentHash;  
 }
